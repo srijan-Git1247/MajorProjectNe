aa545040c8c7ee26060dd4f14ecf476790b4db58
srijan <bagdassrijan@hotmail.com>
2022-03-03 15:53:53 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (<div>
+      <h1> HolyShit</h1>
+        <p>This is an app to Request blood and Donation </p>
+        <p>Version 1.0.0</p>
+      </div>);
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function add() {
+  return (
+    <div>add</div>
+  )
+}
